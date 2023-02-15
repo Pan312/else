719733ff8e0c02e57bea0949d2ac64a3d3db8449
Your Name <you@example.com>
2023-02-15 15:55:03 +0300

    lambda + Pan + com

--- lambda.py
+++ lambda.py
@@ -2,10 +2,19 @@ double = lambda x: x * 2
 print(double(1))
 
 multiply = lambda x, y: x * y
-print(multiply(1,2))
+print(multiply(1, 2))
 
 add = lambda x, y, z: x + y + z
-print(add(1,2,3))
+print(add(1, 2, 3))
 
-full_name = lambda first_name, last_name: first_name+" "+last_name
-print(full_name("Bro","Code"))
+full_name = lambda first_name, last_name: first_name + " " + last_name
+print(full_name("Bro", "Code"))
+
+
+print("Pan")
+<<<<<<< HEAD
+print("Pann")
+
+#hfhffk
+=======
+>>>>>>> 03c4aa3 (lambda + Pan)
