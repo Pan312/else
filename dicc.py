82190f12f9295338d2d6c3b18387242f2b2e8e0c
Your Name <you@example.com>
2023-02-15 16:48:38 +0300

    Revert "first add"
    This reverts commit 32318fd2f5b0265b34250c192acbb6066ff7a2ee.

--- dicc.py
+++
@@ -1,8 +0,0 @@
-capitals = {'USA':'Washington DC','India':'New Dehli'}
-capitals.update({'Germany':'Berlin'})
-#print(capitals.keys())
-#print(capitals.values())
-capitals.update({'USA':'Moscow'})
-
-for key,value in capitals.items():
-    print(key,value)
